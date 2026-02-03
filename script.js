6213ea04a42102e604db3420b6a703dfade50113
Micha3lll0l <112942168+Micha3lll0l@users.noreply.github.com>
2026-02-03 20:30:03 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function clickfunc() {
+    window.location.href = "page-oui.html"
+}
+
+function increaseSize() {
+    const ouiButton = document.querySelector(".oui");
+
+    ouiButton.style.width = (ouiButton.offsetWidth + 70) + 'px';
+    ouiButton.style.height = (ouiButton.offsetHeight + 70) + 'px';
+
+    const currentFontSize = window.getComputedStyle(ouiButton).fontSize;
+    const newFontSize = parseInt(currentFontSize) + 20 + 'px';
+    ouiButton.style.fontSize = newFontSize
+}
